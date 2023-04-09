c285d4f4aab4336413f255deb66eb9bd0727e72f
patjoly <patjoly2@gmail.com>
2023-04-09 15:29:21 -0400

    increment $VERSION after v1.08 release

--- script/fit2tcx.pl
+++ script/fit2tcx.pl
@@ -2,7 +2,7 @@
 use strict;
 use warnings;
 
-our $VERSION = '1.08';
+our $VERSION = '1.09';
 
 =encoding utf-8
 
@@ -913,7 +913,7 @@ Please visit the project page at: L<https://github.com/patjoly/geo-fit>.
 
 =head1 VERSION
 
-1.08
+1.09
 
 =head1 LICENSE AND COPYRIGHT
 
